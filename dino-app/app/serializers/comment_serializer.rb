b5a715325dc70c5515af8c45a7ecce1e26fbc829
laseq <lourenco.sequeira@outlook.com>
2017-05-31 00:52:27 +0100

    Changed Comment serializer to just show the relevant User JSON data

--- dino-app/app/serializers/comment_serializer.rb
+++ dino-app/app/serializers/comment_serializer.rb
@@ -2,4 +2,11 @@ class CommentSerializer < ActiveModel::Serializer
   attributes :id, :body, :user
   has_one :user
   has_one :dinosaur
+
+  def user
+    data = {}
+    data[:id] = object.user.id
+    data[:username] = object.user.username
+    data
+  end
 end
