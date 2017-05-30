8ca47ee2153a33e397ed9e75341d56b955d880f6
LBoyle <louis_boyle@hotmail.com>
2017-05-30 13:52:15 +0100

    populated responses in seralizers

--- dino-app/app/models/dinosaur.rb
+++ dino-app/app/models/dinosaur.rb
@@ -1,2 +1,3 @@
 class Dinosaur < ApplicationRecord
+  has_many :comments
 end
--- dino-app/app/serializers/dinosaur_serializer.rb
+++ dino-app/app/serializers/dinosaur_serializer.rb
@@ -1,3 +1,4 @@
 class DinosaurSerializer < ActiveModel::Serializer
   attributes :id, :name, :image, :period, :description
+  has_many :comments
 end
