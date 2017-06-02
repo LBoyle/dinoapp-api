09f2edeb9ca4e81a3a2c173afe09bef3aad48efb
laseq <lourenco.sequeira@outlook.com>
2017-06-02 10:07:06 +0100

    Readded the comment serializer code. No idea how it disappeared.

--- dino-app/app/serializers/comment_serializer.rb
+++ dino-app/app/serializers/comment_serializer.rb
@@ -3,3 +3,10 @@ class CommentSerializer < ActiveModel::Serializer
   has_one :user
   has_one :dinosaur
 end
+
+def user
+  data = {}
+  data[:id] = object.user.id
+  data[:username] = object.user.username
+  data
+end
